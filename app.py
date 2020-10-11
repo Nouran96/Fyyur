440a59d8267184c74d29073034683896e48d053a
Nouran Samy <nourans.96@yahoo.com>
2020-10-11 19:58:30 +0200

    Edit Artist

--- app.py
+++ app.py
@@ -268,19 +268,8 @@ def show_artist(artist_id):
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
   form = ArtistForm()
-  artist={
-    "id": 4,
-    "name": "Guns N Petals",
-    "genres": ["Rock n Roll"],
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "326-123-5000",
-    "website": "https://www.gunsnpetalsband.com",
-    "facebook_link": "https://www.facebook.com/GunsNPetals",
-    "seeking_venue": True,
-    "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-    "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-  }
+  artist = Artist.query.get(artist_id)
+
   # TODO: populate form with fields from artist with ID <artist_id>
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
@@ -288,6 +277,16 @@ def edit_artist(artist_id):
 def edit_artist_submission(artist_id):
   # TODO: take values from the form submitted, and update existing
   # artist record with ID <artist_id> using the new attributes
+  artist = Artist.query.get(artist_id)
+
+  artist.name = request.form.get('name', '')
+  artist.city = request.form.get('city', '')
+  artist.state = request.form.get('state', '')
+  artist.phone = request.form.get('phone', '')
+  artist.facebook_link = request.form.get('facebook_link', '')
+  artist.genres = (',').join(request.form.getlist('genres'))
+
+  db.session.commit()
 
   return redirect(url_for('show_artist', artist_id=artist_id))
 
