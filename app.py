9865e9027e7be7b7981fc2b1f89b3bc1476f71c9
Nouran Samy <nourans.96@yahoo.com>
2020-10-15 09:38:49 +0200

    Create Artist

--- app.py
+++ app.py
@@ -340,11 +340,19 @@ def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
+  artist = Artist(name = request.form['name'], city = request.form['city'],
+                state = request.form['state'], phone = request.form['phone'], 
+                genres = (',').join(request.form.getlist('genres')),
+                facebook_link = request.form['facebook_link'])
+  try:
+    db.session.add(artist)
+    db.session.commit()
 
   # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    flash('Artist ' + artist.name + ' was successfully listed!')
+  except:
   # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
+    flash('An error occurred. Artist ' + artist.name + ' could not be listed.')
   return render_template('pages/home.html')
 
 
